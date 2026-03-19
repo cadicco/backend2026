5ddf46b8ca7eb200612ce40837e18a9b7352a202
cadicco <cadicco@gmail.com>
2026-03-19 00:29:20 +0000

    Aula Hebert 18/3/26

---
+++ src/form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula teste com o Hebert</title>
+    
+</head>
+<body>
+    <h1>Form - 18/03/26</h1>
+    <hr>
+    <form method="POST" action="recebe.php">
+    Login <input type="text" id="login" name="login"><br>
+    Senha <input type="password" id="senha" name="senha"><br>
+    <input type="submit" value="Validar">
+</form>
+</body>
+</html>
