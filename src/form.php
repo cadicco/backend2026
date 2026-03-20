80f4860a2f3a7cc05900e51eb8d25ab9e4771568
cadicco <cadicco@gmail.com>
2026-03-20 23:09:39 +0000

    modificacao

--- src/form.php
+++ src/form.php
@@ -3,16 +3,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aula teste com o Hebert</title>
-    
+    <title>ALÔ MÃE, TÔ CODANDO!</title>
 </head>
+<footer>Code do @Cadicco</footer>
+<style>
+  body 
+   
+    {
+    background-color: #f0f0f0; /* Fundo cinza claro */
+  }
+  h1 {
+    color: darkblue; /* Título azul escuro */
+  }
+  .texto-destaque {
+    color: rgb(255, 87, 34); /* Texto laranja */
+  }
+  
+</style>
 <body>
-    <h1>Form - 18/03/26</h1>
-    <hr>
-    <form method="POST" action="recebe.php">
-    Login <input type="text" id="login" name="login"><br>
-    Senha <input type="password" id="senha" name="senha"><br>
-    <input type="submit" value="Validar">
-</form>
+    
+    <div class="container-texto">
+        
+        <h1> Form - 18/03/26</h1>
+        <hr>
+        <form method="POST" action="recebe.php">
+    Apelido : <input type="text"><br>    
+    Login : <input type="text" id="login" name="login"><br>
+    Senha : <input type="password" id="senha" name="senha"><br>
+        <br>
+        <input style="color:darkblue;" type="submit" value="V A L I D A R">
+        </form>   
+        
+    </div>
 </body>
 </html>
